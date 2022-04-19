07e226d83d5d198a022e829be030475bbf291204
Guilherme Bracero Gonzales <guibragon@gmail.com>
2022-04-19 17:10:42 -0300

    Altered every file in core

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Cargo, Servico, Funcionario, Ferramentas1, Ferramentas2
 
-# Register your models here.
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'ativo', 'modificado')
+
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('servicos', 'icone', 'ativo', 'modificado')
+
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'ativo', 'modificado')
+
+
+@admin.register(Ferramentas1)
+class Ferramentas1Admin(admin.ModelAdmin):
+    list_display = ('titulo', 'descricao', 'icone', 'ativo', 'modificado')
+
+
+@admin.register(Ferramentas2)
+class Ferramentas2Admin(admin.ModelAdmin):
+    list_display = ('titulo', 'descricao', 'icone', 'ativo', 'modificado')
--- core/views.py
+++ core/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.contrib import messages
 
-# Create your views here.
+from .models import Servico, Funcionario, Ferramentas1, Ferramentas2
+from .forms import ContatoForm
+
+class IndexView(FormView):
+    template_name = "index.html"
+    form_class = ContatoForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        ctx = super(IndexView, self).get_context_data(**kwargs)
+        ctx['servicos'] = Servico.objects.order_by('?').all() # Buscando do banco de dados
+        ctx['funcionarios'] = Funcionario.objects.all()
+        ctx['ferramenta1'] = Ferramentas1.objects.order_by('?').all()
+        ctx['ferramenta2'] = Ferramentas2.objects.order_by('?').all()
+        return ctx
+
+    
+    # As duas funções derivam do FormView, que foi importado na linha 2
+
+    # Caso a mensagem seja enviada:
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request, 'E-mail enviado com sucesso!')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    
+    # Caso a mensagem NÃO seja enviada:
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Erro no envio do E-mail.')
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
